706b806f252a26b6f669b9c9fb64da3ad3d62499
Travis CI User <travis@example.org>
2017-04-19 13:43:27 +0000

    [ci skip] update gh-pages

--- build..alpha..travis-ci.org/app/assets.example.js
+++ build..alpha..travis-ci.org/app/assets.example.js
@@ -291,14 +291,15 @@ utility2-comment -->\n\
         local.assetsDict['/assets.example.js'] =
             local.assetsDict['/assets.example.js'] ||
             local.fs.readFileSync(__filename, 'utf8');
+        // bug-workaround - long $npm_package_buildCustomOrg
+        /* jslint-ignore-begin */
         local.assetsDict['/assets.npmdoc_clone_org_repos.rollup.js'] =
             local.assetsDict['/assets.npmdoc_clone_org_repos.rollup.js'] ||
             local.fs.readFileSync(
-                // buildCustomOrg-hack
-                local.npmdoc_clone_org_repos.__dirname +
-                    '/lib.npmdoc_clone_org_repos.js',
+                local.npmdoc_clone_org_repos.__dirname + '/lib.npmdoc_clone_org_repos.js',
                 'utf8'
             ).replace((/^#!/), '//');
+        /* jslint-ignore-end */
         local.assetsDict['/favicon.ico'] = local.assetsDict['/favicon.ico'] || '';
         // if $npm_config_timeout_exit exists,
         // then exit this process after $npm_config_timeout_exit ms
